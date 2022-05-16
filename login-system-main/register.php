7645bca0cb6955f1be93191fbb729593af1029f0
abhishek khajuria <63535532+abhishekhajuria@users.noreply.github.com>
2022-05-16 19:20:07 +0530

    Update register.php

--- login-system-main/register.php
+++ login-system-main/register.php
@@ -108,7 +108,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <!-- navgation menu start  -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#" style="font-size:30px;"><strong>Coding Cush</strong></a>
+  <a class="navbar-brand" href="#" style="font-size:30px;"><strong>PTU CHESS CLUB</strong></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -116,25 +116,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="http://localhost:8091/ChessClub/index.html">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
+    
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <a href="login.php"class="btn btn-success my-2 my-sm-0" type="submit">Login Account</a>
